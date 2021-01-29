772cddccb087d952d5ecbacde149bfcd61e0683f
Fanny Chan <fanny@byteslice.se>
2021-01-29 10:22:28 +0100

    Added aws cdk for cognito.

---
+++ aws/lib/aws-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from "@aws-cdk/core";
+import {
+  OAuthScope,
+  UserPool,
+  UserPoolClient,
+  UserPoolClientIdentityProvider,
+} from "@aws-cdk/aws-cognito";
+import { Duration } from "@aws-cdk/core";
+export class AwsStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const userPool = new UserPool(this, "userPool", {
+      userPoolName: "quote-machine",
+    });
+
+    new UserPoolClient(this, "userPoolClient", {
+      userPoolClientName: "web",
+      generateSecret: false,
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        callbackUrls: ["http://localhost:3000"],
+        scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.COGNITO_ADMIN],
+      },
+      supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
+      accessTokenValidity: Duration.minutes(60),
+      refreshTokenValidity: Duration.days(1),
+      userPool: userPool,
+    });
+  }
+}
