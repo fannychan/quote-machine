474126e53f649439c97251b6661404e32f1358b6
Fanny Chan <fanny@byteslice.se>
2021-02-04 11:14:51 +0100

    Added first github action.

--- web/src/client.tsx
+++ web/src/client.tsx
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, createHttpLink, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
--- web/src/views/Form.tsx
+++ web/src/views/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { gql, useMutation } from "@apollo/client";
 import { Button } from "../components/Button";
